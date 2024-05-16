1b3897157b0b832ad4c009155e6f0e18b623e998
sandra Gomez <sgomezsi@gmail.com>
2024-05-16 11:44:48 +0200

    creacion de Category y Order

---
+++ app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['description','ubication','price','category_id','n_docu',];
+
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+}
---
+++ database/migrations/2024_05_16_093417_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('n_docu')->nullable();
+            $table->string('description')->nullable();
+            $table->string('ubication')->nullable();
+            $table->integer('price')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->enum('type',
+                              ['Income','Expense']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
