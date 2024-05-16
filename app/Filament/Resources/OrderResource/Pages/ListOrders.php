834417e47a6cba74203e8e9d761745aab600e488
codeMountainEs <codemountain.es@icloud.com>
2024-05-16 19:17:11 +0200

    Order All Income Expense

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -5,10 +5,29 @@ namespace App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+
 
 class ListOrders extends ListRecords
 {
     protected static string $resource = OrderResource::class;
+
+    public function getTabs(): array 
+    {
+        return [
+            'all' => Tab::make(' All Orders'),
+            'ingresos' => Tab::make('Expense')
+                ->modifyQueryUsing(function ($query){
+                    return $query->where('type','Expense');
+                }),
+            'gastos' => Tab::make('Income') ->modifyQueryUsing(function ($query){
+                return $query->where('type','Income');                
+            }),
+
+        ];
+    }
+
+
 
     protected function getHeaderActions(): array
     {
